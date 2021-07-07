9d330ea4de624a934c9fde9e5c197b44e61524ea
Valdeth <alstontravis6@gmail.com>
2021-07-07 00:39:10 -0600

    resumeData page created

---
+++ src/resumeData.js
@@ -0,0 +1,117 @@
+let resumeData = {
+"imagebaseurl": "https://github.com/TravisAlston",
+    "name": "Travis Alston",
+    "role": "Full Stack Developer",
+    "linkedinId": "",
+    "roleDescription": "I create user-focused applications with creative vision using cutting edge technologies across the full stack of development.",
+    "socialLinks": [
+    {
+        "name": "linkedin",
+        "url": "www.linkedin.com/in/travis-alston",
+        "className": "fa fa-linkedin"
+    },
+    {
+        "name": "github",
+        "url": "https://github.com/TravisAlston",
+        "className": "fa fa-github"
+    },
+    
+    ],
+    "aboutme": "I am a Full-Stack Web Developer Graduate at University of Utah| Full Stack Engineer | Back End Specialist |",
+    "address": "West Jordan, Utah",
+    "website": "Website coming soon...",
+    "education": [
+    {
+        "UniversityName": "University of Utah",
+        "specialization": "Full Stack Development",
+        "MonthOfPassing": "July",
+        "YearOfPassing": "2021",
+        "Achievements": "Certificate"
+    },
+
+    ],
+    "work": [
+    {
+        "CompanyName": "FamilySearch International ",
+        "specialization": "Indexing Operations Specialist",
+        "MonthOfLeaving": "January",
+        "YearOfLeaving": "2020",
+        "Achievements": "Generated over 200 published user facing projects and managed active projects by implementing changes based on user feedback. Wrote clear and concise technical instructions for users about how to index complex information from historical documents. Created example images that communicate how to extract important information from primary source documents. Coordinated with evaluation and translation teams to anticipate instructional roadblocks and optimize user experience. Acted as team liaison with software engineers to manage communications between engineering and other writers, troubleshoot new products developments and generate JIRA reports. Represented department on cross-functional team to design custom image and text editing tool used in creating templates"
+    },
+    
+    ],
+    "skillsDescription": "",
+    "skills": [
+    {
+        "skillname": "HTML5"
+    },
+    {
+        "skillname": "CSS"
+    },
+    {
+        "skillname": "JavaScript ES6"
+    },
+    {
+        "skillname": "JQuery"
+    },
+    {
+        "skillname": "Node.js"
+    },
+    {
+        "skillname": "Express.js"
+    },
+    {
+        "skillname": "MongoDB"
+    },
+    {
+        "skillname": "React.js"
+    },
+    
+    
+    ],
+    "portfolio": [
+    {
+        "name": "Run Buddy",
+        "description": "html and css website",
+        "imgurl": "./images/runbuddy.png",
+        "imglink": "https://github.com/TravisAlston/run-buddy"
+    },
+    {
+        "name": "Password Generator",
+        "description": "Generates a password",
+        "imgurl": "./images/password generator.png",
+        "imglink": "https://github.com/TravisAlston/Random-password-generator"
+
+    },
+    {
+        "name": "Git it done!",
+        "description": "Finding a github repository that needs help",
+        "imgurl": "./images/git-it-done.png",
+        "imglink": "https://github.com/TravisAlston/git-it-done"
+
+    },
+    {
+        "name": "Code Quiz",
+        "description": "A code quiz that has a timer and leaderboards",
+        "imgurl": "images/code-quiz.png",
+        "imglink": "https://github.com/TravisAlston/code-quiz"
+
+    },{
+        "name": "To Do List",
+        "description": "Introduced to Bootstrap",
+        "imgurl": "./images/todo-list.png",
+        "imglink": "https://github.com/TravisAlston/Javids-todo-list"
+
+    },
+    {
+        "name": "Zoo Keeper",
+        "description": "Makes a database full of animals and workers in a zoo",
+        "imgurl": "./images/zoo-keeper.png",
+        "imglink": "https://github.com/TravisAlston/zookeepr"
+
+    }
+]
+
+}
+
+export default resumeData
